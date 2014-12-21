35b3bca069f2fb1b1768a0cbc8f3d638470e2e1d
Pritesh <pritesh@Priteshs-MacBook-Pro.local>
2014-12-20 19:18:46 -0500

    Rear end camera working.

--- js/callBacks.js
+++ js/callBacks.js
@@ -4,6 +4,17 @@
 var pictureSource;
 var destinationType; 
 var sourceInfo; 
+var ID;
+
+var constraints = {
+    audio: {
+      optional: [{sourceId: audioSource}]
+    },
+    video: {
+      optional: [{sourceId: videoSource}]
+    }
+  };
+
 
 function onDeviceReady() {
 
@@ -11,9 +22,11 @@ function onDeviceReady() {
 	var vid = document.getElementById('video');
 	
 	if(navigator.getUserMedia) {
+		alert(' in getUserMedia' );
 
-		navigator.getUserMedia(	{video: true}, 
-		
+
+		navigator.getUserMedia(	{ video: {  optional: [{sourceId: ID}] } }, 
+
 				function (stream) {
 					
 					alert(' no chaman' );
@@ -32,8 +45,23 @@ function onDeviceReady() {
 		alert('no browser support! Go HOme!');
 	}
 
+}
 
+function gotSources(sourceInfos)
+{
+    for (var i = 0; i < sourceInfos.length; i++)
+    {
+        var sourceInfo = sourceInfos[i];
 
+        if (sourceInfo.facing == 'environment')
+        {
+            ID = sourceInfo.id;
+            alert(ID);
+
+        }
+    }
+
+    onDeviceReady();
 }
 
 
